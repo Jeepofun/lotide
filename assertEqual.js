3de92a4df21578f80db4e19219fe5acfc042eafe
Don Miller <doncmiller@live.com>
2021-10-25 21:30:11 -0700

    tail

--- assertEqual.js
+++ assertEqual.js
@@ -3,5 +3,18 @@ const assertEqual = function(actual, expected) {
     console.log(`✅✅✅ Assertion Passed:  ${expected} === ${expected}`);
   else console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 2);
+function head(a) {
+  let x = a[0];
+  return x;
+}
+function tail(arr) {
+  return arr.slice(1);
+}
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
