4013206ec4d5bb847b5ac5eea79770702b48185c
Don Miller <doncmiller@live.com>
2021-10-26 23:17:01 -0700

    add without

---
+++ without.js
@@ -0,0 +1,54 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`✅✅✅ Assertion Passed:  ${expected} === ${expected}`);
+  else console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+function head(a) {
+  let x = a[0];
+  return x;
+}
+function tail(arr) {
+  return arr.slice(1);
+}
+const eqArrays = function(arr1, arr2) {
+  let output = true;
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i ++) {
+    if (Array.isArray(arr1[i]) || Array.isArray(arr2[i])) {
+      output = output && eqArrays(arr1[i], arr2[i]);
+    } else if (arr1[i] !== arr2[i]) {
+      output = output && false;
+    }
+  }
+  return output;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed:  ${arr1} === ${arr2}`)
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arr2} !== ${arr2}`)
+  }
+}
+// code may be lacking here
+
+const without = function(arr1, arr2) {
+  let filteredItems = [];
+  for (const item of arr1) {
+    if (!arr2.includes(item)) {
+      filteredItems.push(item)
+    }
+  }
+  return filteredItems
+}
+// not sure how to test the without function!
+console.log(without([1, 2, 3], [1]))
+console.log(without(["1", "2", "3"], [1, 2, "3"]))
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+([1, 2, 3], [1, 2, "4"])
+
