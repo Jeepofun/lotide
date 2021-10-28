5ff92f38aec7b9c80d285591925c89b323ce7fb0
Don Miller <doncmiller@live.com>
2021-10-27 18:53:26 -0700

    removed console.logs

--- letterPositions.js
+++ letterPositions.js
@@ -12,8 +12,6 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const assertArraysEqual = function(arr1, arr2) {
-  console.log(arr1)
-  console.log(arr2)
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed:  ${arr1} === ${arr2}`)
   } else {
